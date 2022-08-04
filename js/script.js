d15fe49e335ebdf3b2b797babd45fd4b0ab5b23b
TILIKU <n.eriashvili12@gmail.com>
2022-08-04 22:18:02 +0400

    i think i finnished everything literally outplayed fever lol

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
 $(document).ready(function(){
 
+// navbar and stuff not weird fancy stuff from github lol
+
+// navbar toggler btn
+$('.navbar-toggler').click(function(){
+
+  $('.navbar-toggler').toggleClass('change')
+
+})
+// sticky navbar
+$(window).scroll(function(){
+
+  let position = $(this).scrollTop();
+
+  if(position>=718){
+    $('.navbar').addClass('navbar-background');
+    $('.navbar').addClass('fixed-top');
+  }
+  else{
+    $('.navbar').removeClass('navbar-background');
+    $('.navbar').removeClass('fixed-top');
+  }
+
+})
+
+
+//back to top btn
+$(window).scroll(function(){
+
+  let position = $(this).scrollTop();
+
+  if(position>=718){
+    $('#back-to-top').addClass('scroll-top');
+  }
+  else{
+    $('#back-to-top').removeClass('scroll-top');
+  }
+
+})
+
+
+
+
+//==================================================================
 $("header, .info").ripples({
     dropRadius: 15,
     perturbance:0.01 ,
